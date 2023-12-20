68846c309bbd08cfdd21e969d6761882f67a3fc5
Boris <Glazunov2142@icloud.com>
2023-12-20 21:13:12 +0300

    chore: improved ddos.

--- src/model/ddos.rs
+++ src/model/ddos.rs
@@ -1,11 +1,11 @@
 #[derive(Debug, Clone)]
 pub struct DDOS {
     pub url: String,
-    pub workers_num: i8
+    pub workers_num: i32
 }
 
 impl DDOS {
-    pub fn new(url: String, workers_num: i8) -> Self {
+    pub fn new(url: String, workers_num: i32) -> Self {
         return DDOS{url, workers_num}
     }
 }
